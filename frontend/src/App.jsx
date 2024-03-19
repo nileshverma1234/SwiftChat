07a082c19e7054a97d73713377abd5a76c582ab2
Nilesh <nv63354@gmail.com>
2024-03-19 23:19:34 +0530

    Login page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
 import './App.css'
+import Login from './pages/login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Login/>
+      
     </div>
+
+    
   )
 }
 
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                Login
+                <span className='text-blue-500'> SwiftChat</span>
+            </h1>
+
+            <form >
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Username or Email</span>
+                    </label>
+
+                    <input 
+                        type="text"
+                        placeholder='Enter username or Email'
+						className='w-full input input-bordered h-10'
+                     />
+
+                </div>
+
+                <div>
+                    <label className='label'>
+						<span className='text-base label-text'>Password</span>
+					</label>
+
+                    <input
+						type='password'
+						placeholder='Enter Password'
+                        className='w-full input input-bordered h-10'
+						
+					/>
+
+                </div>
+                {/* Dont have link part */}
+
+                <div>
+					<button className='btn btn-block btn-sm mt-2' >Login</button>
+				</div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
